e879e47a4aeae87adfc83798cc3552fee4874b4c
CamiloMD21 <117365650+CamiloMD21@users.noreply.github.com>
2024-03-12 18:19:58 -0500

    Commit inicial

---
+++ src/main/java/co/com/parcial/middlewaregraphql/MiddlewareGraphqlApplication.java
@@ -0,0 +1,13 @@
+package co.com.parcial.middlewaregraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddlewareGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/parcial/middlewaregraphql/MiddlewareGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.parcial.middlewaregraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddlewareGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
