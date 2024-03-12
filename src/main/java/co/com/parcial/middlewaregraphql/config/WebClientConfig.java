be16cd52e7d72850090e4a77d4dac1f4fa6274eb
CamiloMD21 <117365650+CamiloMD21@users.noreply.github.com>
2024-03-12 18:23:35 -0500

    Inicial

--- src/main/java/co/com/parcial/middlewaregraphql/config/WebClientConfig.java
+++ src/main/java/co/com/parcial/middlewaregraphql/config/WebClientConfig.java
@@ -2,7 +2,6 @@ package co.com.parcial.middlewaregraphql.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
